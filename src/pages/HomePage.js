0fc0a1a769a98421306104457d383ffa8b2d6edf
Pardeep Sanghera <pradeepsanghera@gmail.com>
2021-05-07 11:02:19 -0700

    feat(axios) imported axios

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,65 @@
+import { useState} from 'react';
+import instance from '../api/apiConfig';
 
 const HomePage = () => {
+    const [showBooks, setShowBooks]= useState([])
+
+    const fetchBooks = async () => {
+        try{
+            let {data} = await instance.get ('/api/books');
+
+        }
+        catch (e){
+            console.log(e)
+        }
+    }
     return (
         <div id='home-page'>
-            <h2>GOT</h2>
+            <div className='row text-center mt-3'>
+              <div className='col'>
+                  <h1>Game of Thrones Book List!</h1>
+              </div>
+            </div>
+            {/** show button */}
+            <div className='row text-center mt-3'>
+              <div className='col'>
+                <button className='btn btn-primary' onClick={()=>fetchBooks()}>
+                    Show
+                </button>
+               </div>
+            </div>
+            {/**Search filter */}
+            <div className='row mt-5'>
+        <div className='col'>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='hero-search'
+              placeholder='Search for Game of thrones book'
+              value=''
+              onChange=''
+            />
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+      
+          {showBooks.map((book,index) => {
+              return (
+                <div className='col-sm-12 col-md-3 mt-3'>
+                  {/* <BookCards book={book}/> */}
+                  </div>
+
+              )
+          })}
+      
+
+      </div>
+      
 
         </div>
-
+        
 
     )
 }
